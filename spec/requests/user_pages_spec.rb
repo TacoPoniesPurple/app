6bfcd067bb134206243fb9c6c3c67e97f0af9cbc
Edgar Hiram Hernández Reyes <tacoponiespurple@gmail.com>
2013-05-25 19:10:08 -0600

    Finalizado capitulo 7

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -28,7 +28,14 @@ describe "UserPages" do
     describe "with invalid information" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
-        end
+      end
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_selector('title', content: 'Registro') }
+        it { should have_content('error') }
+      end
     end
 
     describe "with valid information" do
@@ -41,6 +48,14 @@ describe "UserPages" do
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email('hazel@feathers.com') }
+
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Bienvenido') }
       end
     end
   end
