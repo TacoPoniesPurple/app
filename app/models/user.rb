aa7dabf16783bdbdcd5fd16b68173f0c023230d9
Edgar Hiram Hernández Reyes <tacoponiespurple@gmail.com>
2013-05-28 21:11:34 -0600

    Agregando acciones de Follow

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,13 @@
 
 class User < ActiveRecord::Base
   has_many :microposts, dependent: :destroy
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+  has_many :reverse_relationships, foreign_key: "followed_id",
+           class_name:  "Relationship",
+           dependent:   :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
+
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
 
@@ -31,6 +38,19 @@ class User < ActiveRecord::Base
   def feed
     # Codigo incompleto, falta "Following users"
     Micropost.where("user_id = ?", id)
+  end
+
+  def following?(other_user)
+    relationships.find_by_followed_id(other_user.id)
+  end
+
+  def follow!(other_user)
+    #self.relationships.create!(followed_id: other_user.id)
+    relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    relationships.find_by_followed_id(other_user.id).destroy
   end
 
   private
